ef7c65612fefff7d2b55ad27b1969cbbe25b23a2
tracyhnguyen <21tnguyen28@gmail.com>
2025-10-13 10:56:37 -0700

    added settings-profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/home/home';
 import { SettingsAccountComponent } from './pages/settings-account/settings-account.component';
 import { Signup } from './pages/signup/signup';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { SettingsProfileComponent } from './pages/settings-profile/settings-profile.component';
 
 export const routes: Routes = [
   { path: '', component: Home }, //default route
+  { path: 'signup', component: Signup }, //signup page
+  { path: 'main-page', component: MainPageComponent } ,//main-page
   { path: 'settings-account', component: SettingsAccountComponent }, //settings-accounts page
-  { path: 'signup', component: Signup},
-  { path: 'main-page', component: MainPageComponent}
+  { path: 'settings-profile', component: SettingsProfileComponent } //settings-profile page
 ];
 export class AppRoutingModule { }
