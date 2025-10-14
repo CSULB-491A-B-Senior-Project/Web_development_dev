9d33c3e55d8c98770b89bb1c9f068914f6a8a8d4
tracyhnguyen <21tnguyen28@gmail.com>
2025-10-13 21:21:56 -0700

    added icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SettingsAccountComponent } from './pages/settings-account/settings-acco
 import { Signup } from './pages/signup/signup';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SettingsProfileComponent } from './pages/settings-profile/settings-profile.component';
+import { ArtistComponent } from './pages/artist/artist.component';
 
 export const routes: Routes = [
   { path: '', component: Home }, //default route
   { path: 'signup', component: Signup }, //signup page
   { path: 'main-page', component: MainPageComponent } ,//main-page
+  { path: 'artist', component: ArtistComponent }, //artist page
   { path: 'settings-account', component: SettingsAccountComponent }, //settings-accounts page
   { path: 'settings-profile', component: SettingsProfileComponent } //settings-profile page
 ];
