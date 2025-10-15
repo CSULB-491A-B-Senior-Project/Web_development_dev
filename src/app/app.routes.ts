2097084c297b35351740854ca56567d57a1e2524
Travis Campbell <traviscampbell7777@gmail.com>
2025-10-15 14:25:01 -0700

    SCRUM-53

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { SettingsAccountComponent } from './pages/settings-account/settings-account.component';
 import { Signup } from './pages/signup/signup';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 export const routes: Routes = [
   { path: '', component: Home }, //default route
   { path: 'settings-account', component: SettingsAccountComponent }, //settings-accounts page
-  { path: 'signup', component: Signup},
+  { path: 'signup', component: Signup },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'explore',
+        loadComponent: () => import('./pages/explore/explore')
+          .then(m => m.Explore)
+      },
+    ]
+  },
 ];
 export class AppRoutingModule { }
