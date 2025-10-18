b3802989c3d4ee6ab217e06b97e7787a2d21079e
gracekflores <168140240+gracekflores@users.noreply.github.com>
2025-10-18 16:01:37 -0700

    Added character limiting and resizing
    The text box now only resizes to fit the text, and only 150 characters are allowed as specificied in the use case. Underneath it to the right, the character limit is displayed. I also changed the submit button alignment to the right as well.

--- src/app/pages/settings-profile/settings-profile.component.ts
+++ src/app/pages/settings-profile/settings-profile.component.ts
@@ -8,15 +8,23 @@ import { RouterLink } from '@angular/router';
   selector: 'app-settings-profile',
   templateUrl: './settings-profile.component.html',
   styleUrls: ['./settings-profile.component.scss'],
-  imports: [RouterLink, ReactiveFormsModule]
+  imports: [RouterLink, ReactiveFormsModule, FormsModule]
 })
 export class SettingsProfileComponent {
-  editableText = new FormControl(''); // initial empty or default value
+  editableText = new FormControl('');
+
+  text: string = '';
+
+  // auto resizing the box to fit the text
+  autoResize(event: Event): void {
+    const textarea = event.target as HTMLTextAreaElement;
+    textarea.style.height = 'auto'; // reset height
+    textarea.style.height = textarea.scrollHeight + 'px'; // set to scroll height
+  }
 
   onSubmit() {
     const submittedText = this.editableText.value;
     this.editableText.setValue(submittedText); // reassigns the value (optional here)
-    // console.log('Submitted:', submittedText);
   }
 
 }
