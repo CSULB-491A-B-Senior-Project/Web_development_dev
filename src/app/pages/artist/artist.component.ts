af3c530b3de23703a55ad8e05512f889497a2af1
tracyhnguyen <21tnguyen28@gmail.com>
2025-10-27 12:24:05 -0700

    added chips to sort menu of artist page

--- src/app/pages/artist/artist.component.ts
+++ src/app/pages/artist/artist.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AlbumCard } from '../../ui/album-card/album-card';
 
 type Item = {
     id: number;
+    title: string;
     genres: string[];
     dateLabel: string;
     imageUrl: string;
@@ -12,36 +14,41 @@ type Item = {
     selector: 'app-artist',
     templateUrl: './artist.component.html',
     styleUrls: ['./artist.component.scss'],
-    imports: [RouterLink, AlbumCard]
+    imports: [RouterLink, AlbumCard, CommonModule]
 })
 export class ArtistComponent {
     items = signal<Item[]>([
     {
         id: 1,
+        title: 'Title',
         genres: ['hip-hop', 'lofi', 'indie', 'pop'],
         dateLabel: 'Today',
         imageUrl: 'https://picsum.photos/seed/card-1/600/600'
     },
     {
         id: 2,
+        title: 'Title',
         genres: ['ambient', 'electronic'],
         dateLabel: 'Yesterday',
         imageUrl: 'https://picsum.photos/seed/card-2/600/600'
     },
     {
         id: 3,
+        title: 'Title',
         genres: ['ambient', 'electronic', 'metal'],
         dateLabel: '2d',
         imageUrl: 'https://picsum.photos/seed/card-3/600/600'
     },
     {
         id: 4,
+        title: 'Title',
         genres: ['rap'],
         dateLabel: '1w',
         imageUrl: 'https://picsum.photos/seed/card-4/600/600'
     },
     {
         id: 5,
+        title: 'Title',
         genres: ['ambient', 'edm'],
         dateLabel: '11w',
         imageUrl: 'https://picsum.photos/seed/card-5/600/600'
