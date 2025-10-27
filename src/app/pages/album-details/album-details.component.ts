f6ed3c0dbdb194d99225e3e40cb82a33774bc04d
gracekflores <168140240+gracekflores@users.noreply.github.com>
2025-10-27 13:10:54 -0700

    added link from album-details to main page
    was having a bit of trouble with this initially, but i realized that you must add the routerlink module to the component file for any routing with routerlink to work

--- src/app/pages/album-details/album-details.component.ts
+++ src/app/pages/album-details/album-details.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-album-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './album-details.component.html',
   styleUrl: './album-details.component.scss'
 })
