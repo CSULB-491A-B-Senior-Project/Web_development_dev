1bd1eb5ad6a9a9bcab06ab18a1b1cfae91f6bd5b
tracyhnguyen <21tnguyen28@gmail.com>
2025-11-13 16:59:03 -0800

    Admin mod page skeleton

---
+++ src/app/pages/admin/admin.ts
@@ -0,0 +1,9 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+    selector: 'app-admin',
+    templateUrl: './admin.html',
+    styleUrls: ['./admin.scss'],
+    imports:[]
+})
+export class Artist {}
