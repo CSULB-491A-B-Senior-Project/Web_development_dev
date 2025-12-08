6aad229c5d5584772c0af730ba8527416ba47f75
William Murray <wemx836@gmail.com>
2025-12-07 21:56:18 -0800

    still having issues testing on my end. need to get https to backend and frontend... using ngrok to some success

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 1. Build using the specific Dev Dockerfile
 docker build -f Dockerfile.dev -t crescendo-dev .
-docker run -p 4200:4200 --add-host=host.docker.internal:host-gateway crescendo-dev
+docker run -p 4200:4200 --add-host=host.docker.internal:host-gateway --rm crescendo-dev 
 wait
 
